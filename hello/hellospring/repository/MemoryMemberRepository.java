5aa9d6c90de1b86aa52c8632a6d0cd345f63a877
ratellll <latellbin@naver.com>
2023-08-08 22:48:10 +0900

    MemoreyMemberRepository 만들고 기능 만들기
    sequence 만들어줘서 Long id 배정해주고 (save기능에)
    Optional로 null을 감싸준후 반환한다

---
+++ hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,35 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static Long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+         member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // null이 반환될 가능성이 있으면 Optional로 감싸서 반환하는 것이 좋음
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny(); // 하나라도 찾으면 반환
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values()); // store에 있는 모든 값들을 반환
+    }
+}
